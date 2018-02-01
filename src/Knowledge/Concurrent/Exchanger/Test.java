53ff01f08d92af08ab0ea151d45ceb657069b4e1
hw <hw@ciphergateway.com>
2018-02-01 20:11:29 +0800

    add java concurrent exchanger

--- src/Knowledge/Concurrent/Exchanger/Test.java
+++ src/Knowledge/Concurrent/Exchanger/Test.java
@@ -1,8 +1,45 @@
 package Knowledge.Concurrent.Exchanger;
 
+import java.util.Random;
 import java.util.concurrent.Exchanger;
 
+/**
+ * @author hw 2018.2.1
+ * swap object between two threads 线程间通信
+ */
 public class Test {
 
-  Exchanger exchanger = null;
+  private static class ExchangerRunnable implements Runnable {
+    private Exchanger<String> exchanger = null;
+    private String string = null;
+
+    public ExchangerRunnable(Exchanger<String> exchanger, String string) {
+      this.exchanger = exchanger;
+      this.string = string;
+    }
+
+    @Override
+    public void run() {
+      String name = Thread.currentThread().getName();
+      System.out.println(name + " : before exchange : " + string);
+      try {
+        Random random = new Random();
+        int time = random.nextInt(1000) + 500;
+        Thread.sleep(time);
+        String result = exchanger.exchange(string);
+        System.out.println(name + " : after exchange : " + result);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Exchanger<String> exchanger = new Exchanger<String>();
+    ExchangerRunnable e1 = new ExchangerRunnable(exchanger, "hanwei");
+    ExchangerRunnable e2 = new ExchangerRunnable(exchanger, "zhenbang");
+
+    new Thread(e1, "hanwei").start();
+    new Thread(e2, "zhenbang").start();
+  }
 }
